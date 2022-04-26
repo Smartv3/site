cd0997cb9f45f8ae0909d64c9247024ef643262c
myapp <m.ibrahim@smartvisionstore.com>
2022-04-26 22:14:31 +0300

    create build 11

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import ErrorBoundary from "./helper/ErrorBoundary";
 // import reportWebVitals from './reportWebVitals';
 
 // Before
-// Build 10
+// Build 11
 import "./i18n";
 ReactDOM.render(
   <React.StrictMode>
