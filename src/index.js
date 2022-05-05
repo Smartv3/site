0032af82fc9b7a1740db45fec39972dacb1668ef
myapp <m.ibrahim@smartvisionstore.com>
2022-05-05 09:07:25 +0300

    create build 18

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import CursorContextProvider from "./helper/CursorContextProvider";
 // import reportWebVitals from './reportWebVitals';
 
 // Before
-// Build 17
+// Build 18
 import "./i18n";
 ReactDOM.render(
   <React.StrictMode>
