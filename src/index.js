1f61f7434924129943e02f9cf0215cc24a8d1d08
myapp <m.ibrahim@smartvisionstore.com>
2022-05-15 13:51:58 +0300

    create build 39

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { createRoot } from "react-dom/client";
 import "./i18n";
 
 // After
-// Build 38
+// Build 39
 
 const container = document.getElementById("root");
 const root = createRoot(container); // createRoot(container!) if you use TypeScript
